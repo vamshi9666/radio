0a6133fe7abb0141b68b3b712560ae80d424a30a
vamshi9666 <vamshi9666@riseup.net>
2018-09-28 17:45:09 +0530

    eslint with prettier

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { createServer } = require('http');
 
-const {parse} = require('url');
+const  { parse } = require('url');
 
 // eslint-disable-next-line
 const dev = process.env.NODE_ENV !== 'prod';
@@ -11,28 +11,25 @@ const app = next({dev});
 
 const handle = app.getRequestHandler();
 
-
 // eslint-disable-next-line
 const port = process.env.port || 3000;
 
 app.prepare().then(() => {
-	createServer((req, res) => {
-		const parsedUrl = parse(req.url,true);
-		const { pathname, query } = parsedUrl;
-		if (pathname === '/') {
-			app.render(req, res, '/', query);
-		} else if (pathname =='/a'){a
-			app.render(req,res,'/a', query);
-		} else {
-			handle(req, res, parsedUrl);
-		}
-	})
-		.listen(port, (err) => {
-			if (err){
-				console.log(err);
-			}
-			else{
-				console.log(`Server started on http://localhost:${port}`);
-			}
-		});
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const {pathname, query} = parsedUrl;
+    if (pathname === '/') {
+      app.render(req, res, '/', query);
+    } else if (pathname == '/a') {
+      app.render(req, res, '/a', query);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(port, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Server started on http://localhost:${port}`);
+    }
+  });
 });
